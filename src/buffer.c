ba585c84f6530e5724582f2e524251e39c5909c5
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2015-10-26 03:28:31 +0900

    More nits for proper LP64BE fixes.

--- src/buffer.c
+++ src/buffer.c
@@ -249,7 +249,7 @@ void
 reset_buffer_local_variables(b)
      register struct buffer *b;
 {
-  register int offset;
+  register Lisp_Object_Int offset;
 
   /* Reset the major mode to Fundamental, together with all the
      things that depend on the major mode.
@@ -269,7 +269,7 @@ reset_buffer_local_variables(b)
 
   for (offset = (char *)&buffer_local_flags.name - (char *)&buffer_local_flags;
        offset < sizeof (struct buffer);
-       offset += sizeof (Lisp_Object)) /* sizeof int == sizeof Lisp_Object */
+       offset += sizeof (Lisp_Object))
     if (*(Lisp_Object *)(offset + (char *) &buffer_local_flags) > 0
 	|| *(Lisp_Object *)(offset + (char *) &buffer_local_flags) == -2)
       *(Lisp_Object *)(offset + (char *)b) =
@@ -386,13 +386,13 @@ does not change the local values.")
 
   /* Add on all the variables stored in special slots.  */
   {
-    register int offset, mask;
+    register Lisp_Object_Int offset, mask;
 
     for (offset = (char *)&buffer_local_symbols.name - (char *)&buffer_local_symbols;
 	 offset < sizeof (struct buffer);
 	 offset += (sizeof (Lisp_Object)))
       {
-	mask = *(Lisp_Object *)(offset + (char *) &buffer_local_flags);
+	mask = *(Lisp_Object_Int *)(offset + (char *) &buffer_local_flags);
 	if (mask == -1 || (buf->local_var_flags & mask))
 	  if (XTYPE (*(Lisp_Object *)(offset + (char *)&buffer_local_symbols))
 	      == Lisp_Symbol)
